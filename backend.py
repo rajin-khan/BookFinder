75532329e871db75a284d70ae537024608d3ac6a
Rajin Khan <rajin.khan2001@gmail.com>
2025-02-06 21:58:40 +0600

    Fix backend

--- backend.py
+++ backend.py
@@ -32,4 +32,7 @@ async def save_links(request: SaveLinksRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
